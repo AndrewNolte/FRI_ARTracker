dc9115d04f40bd8e3964cc8087c444058e096680
Andrew Nolte <anolte512@gmail.com>
2019-04-23 14:06:45 -0500

    changed orienttion stuff

--- src/NavPR.cpp
+++ src/NavPR.cpp
@@ -5,64 +5,68 @@
 
 #include <iostream>
 
-NavPR::NavPR(MoveBaseClient &ac, ros::NodeHandle *node) :
-	actionClient(ac),
-	pubGoalPose(node->advertise<geometry_msgs::PoseStamped>("follower_goal_pose", 1)) {}
-
-void NavPR::receivePose(const geometry_msgs::Pose &pose) {
-	std::cout << "NavPR callback triggered" << std::endl;
-
-	// Determine quaternion orientation to turn to
-	/*double turnAngle = atan2(pose.pose.position.y, pose.pose.position.x);
-	tf::Quaternion quat;
-	quat.setRPY(0, 0, turnAngle * 0.25);
-	geometry_msgs::Quaternion goalOrientation;
-	tf::quaternionTFToMsg(quat, goalOrientation);*/
-
-	// Determine position to drive to
-	geometry_msgs::PoseStamped goalPose;
-	goalPose.pose.position.x = pose.position.x;
-	goalPose.pose.position.y = pose.position.y;
-	goalPose.pose.position.z = pose.position.z;
-
-	move_base_msgs::MoveBaseGoal goal;
-	goal.target_pose = goalPose;
-	
-	goal.target_pose.header.frame_id = "base_link";
-	goal.target_pose.header.stamp = ros::Time::now();
-	
-	double angle = atan2(pose.position.z, pose.position.x);
-  tf::Quaternion direction;
-  direction.setRPY(0, 0, 0.2 * angle);
-  goal.target_pose.pose.orientation.w = direction.w();
- 	goal.target_pose.pose.orientation.x = direction.x();
-  goal.target_pose.pose.orientation.y = direction.y();
-  goal.target_pose.pose.orientation.z = direction.z();
-	
-	actionClient.sendGoal(goal);
-	actionClient.waitForResult();
+NavPR::NavPR(MoveBaseClient& ac, ros::NodeHandle* node)
+    : actionClient(ac)
+    , pubGoalPose(node->advertise<geometry_msgs::PoseStamped>("follower_goal_pose", 1))
+{
 }
 
-void NavPR::navCb(const geometry_msgs::PoseStamped &pose) {
-	std::cout << "NavPR callback triggered" << std::endl;
+void NavPR::receivePose(const geometry_msgs::Pose& pose)
+{
+    std::cout << "NavPR callback triggered" << std::endl;
 
-	// Determine quaternion orientation to turn to
-	/*double turnAngle = atan2(pose.pose.position.y, pose.pose.position.x);
+    // Determine quaternion orientation to turn to
+    /*double turnAngle = atan2(pose.pose.position.y, pose.pose.position.x);
 	tf::Quaternion quat;
 	quat.setRPY(0, 0, turnAngle * 0.25);
 	geometry_msgs::Quaternion goalOrientation;
 	tf::quaternionTFToMsg(quat, goalOrientation);*/
 
-	// Determine position to drive to
-	geometry_msgs::PoseStamped goalPose;
-	goalPose.pose.position.x = pose.pose.position.x;
-	goalPose.pose.position.y = pose.pose.position.y;
-	goalPose.pose.position.z = 0;
-	goalPose.pose.orientation = pose.pose.orientation;
-	goalPose.header = pose.header;
+    // Determine position to drive to
+    geometry_msgs::PoseStamped goalPose;
+    goalPose.pose.position.x = pose.position.z;
+    goalPose.pose.position.y = -pose.position.x;
+    goalPose.pose.position.z = 0;
 
-	// Transform pose to robot frame
-	/*tf::TransformListener listener;
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose = goalPose;
+
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    double angle = atan2(pose.position.z, pose.position.x);
+    tf::Quaternion direction;
+    direction.setRPY(0, 0, 0.2 * angle);
+    goal.target_pose.pose.orientation.w = 1; //direction.w();
+    goal.target_pose.pose.orientation.x = 0;
+    goal.target_pose.pose.orientation.y = 0;
+    goal.target_pose.pose.orientation.z = 0;
+
+    actionClient.sendGoal(goal);
+    actionClient.waitForResult();
+}
+
+void NavPR::navCb(const geometry_msgs::PoseStamped& pose)
+{
+    std::cout << "NavPR callback triggered" << std::endl;
+
+    // Determine quaternion orientation to turn to
+    /*double turnAngle = atan2(pose.pose.position.y, pose.pose.position.x);
+	tf::Quaternion quat;
+	quat.setRPY(0, 0, turnAngle * 0.25);
+	geometry_msgs::Quaternion goalOrientation;
+	tf::quaternionTFToMsg(quat, goalOrientation);*/
+
+    // Determine position to drive to
+    geometry_msgs::PoseStamped goalPose;
+    goalPose.pose.position.x = pose.pose.position.x;
+    goalPose.pose.position.y = pose.pose.position.y;
+    goalPose.pose.position.z = 0;
+    goalPose.pose.orientation = pose.pose.orientation;
+    goalPose.header = pose.header;
+
+    // Transform pose to robot frame
+    /*tf::TransformListener listener;
 
 	tf::StampedTransform transform;
 	listener.lookupTransform("camera_link", "base_link", ros::Time(0), transform);
@@ -70,33 +74,34 @@ void NavPR::navCb(const geometry_msgs::PoseStamped &pose) {
 
 	pubGoalPose.publish(goalPose);*/
 
-	move_base_msgs::MoveBaseGoal goal;
-	goal.target_pose = goalPose;
-	actionClient.sendGoal(goal);
-	actionClient.waitForResult();
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose = goalPose;
+    actionClient.sendGoal(goal);
+    actionClient.waitForResult();
 }
 
-void NavPR::transformPose(tf::StampedTransform &transform, geometry_msgs::PoseStamped &pose) {
-	// Translate
-	tf::Vector3 tfOrigin = transform.getOrigin();
+void NavPR::transformPose(tf::StampedTransform& transform, geometry_msgs::PoseStamped& pose)
+{
+    // Translate
+    tf::Vector3 tfOrigin = transform.getOrigin();
 
-	Eigen::MatrixXd position(3, 1);
-	position << pose.pose.position.x, pose.pose.position.y, pose.pose.position.z;
+    Eigen::MatrixXd position(3, 1);
+    position << pose.pose.position.x, pose.pose.position.y, pose.pose.position.z;
 
-	position(0, 0) += tfOrigin.getX();
-	position(1, 0) += tfOrigin.getY();
-	position(2, 0) += tfOrigin.getZ();
+    position(0, 0) += tfOrigin.getX();
+    position(1, 0) += tfOrigin.getY();
+    position(2, 0) += tfOrigin.getZ();
 
-	// Rotate
-	tf::Quaternion tfQuat = transform.getRotation();
-	geometry_msgs::Quaternion rosQuat;
-	tf::quaternionTFToMsg(tfQuat, rosQuat);
-	Eigen::Quaterniond rot(rosQuat.w, rosQuat.x, rosQuat.y, rosQuat.z);
+    // Rotate
+    tf::Quaternion tfQuat = transform.getRotation();
+    geometry_msgs::Quaternion rosQuat;
+    tf::quaternionTFToMsg(tfQuat, rosQuat);
+    Eigen::Quaterniond rot(rosQuat.w, rosQuat.x, rosQuat.y, rosQuat.z);
 
-	position = rot.normalized().toRotationMatrix() * position;
+    position = rot.normalized().toRotationMatrix() * position;
 
-	// Update pose reference
-	pose.pose.position.x = position(0, 0);
-	pose.pose.position.y = position(1, 0);
-	pose.pose.position.z = position(2, 0);
+    // Update pose reference
+    pose.pose.position.x = position(0, 0);
+    pose.pose.position.y = position(1, 0);
+    pose.pose.position.z = position(2, 0);
 }
--- src/hw5_nav_node.cpp
+++ src/hw5_nav_node.cpp
@@ -1,39 +1,42 @@
-#include "ros/ros.h"
 #include "hw5/AlvarMarker.h"
-#include "hw5/TFBroadcastPR.h"
 #include "hw5/NavPR.h"
+#include "hw5/TFBroadcastPR.h"
+#include "ros/ros.h"
 #include <iostream>
 
-#include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
 
-int main(int argc, char **argv) {
-  std::cout << "In startup..." << std::endl;
+int main(int argc, char** argv)
+{
+    std::cout << "In startup..." << std::endl;
 
-  ros::init(argc, argv, "hw5_nav_node");
-  ros::NodeHandle node;
+    ros::init(argc, argv, "hw5_nav_node");
+    ros::NodeHandle node;
 
-  std::cout << "Contacting robot base..." << std::endl;
-	
-  MoveBaseClient ac("move_base", true);
-  while(!ac.waitForServer(ros::Duration(5.0)));
+    std::cout << "Contacting robot base..." << std::endl;
 
-  std::cout << "Building PR chain..." << std::endl;
+    MoveBaseClient ac("move_base", true);
+    while (!ac.waitForServer(ros::Duration(5.0)))
+        ;
 
-  std::string tfbTopicName = "transformed_marker";
+    std::cout << "Building PR chain..." << std::endl;
 
-	NavPR navPr(ac, &node);
-  //TFBroadcastPR tfb(tfbTopicName, &node, navPr);
-  tf::TransformListener tfl;
-  AlvarMarker am(node, tfl, navPr, "nav_kinect_rgb_optical_frame");
+    std::string tfbTopicName = "transformed_marker";
 
-  // node.subscribe(tfbTopicName, 1, &NavPR::navCb, &navPr);
+    NavPR navPr(ac, &node);
+    //TFBroadcastPR tfb(tfbTopicName, &node, navPr);
+    tf::TransformListener tfl;
+    AlvarMarker am(node, tfl, navPr, "nav_kinect_rgb_optical_frame"); //robot
+    //   AlvarMarker am(node, tfl, navPr, "nav_kinect
 
-  std::cout << "Spinning up..." << std::endl;
+    // node.subscribe(tfbTopicName, 1, &NavPR::navCb, &navPr);
 
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-  ros::waitForShutdown();
+    std::cout << "Spinning up..." << std::endl;
 
-  return 0;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    ros::waitForShutdown();
+
+    return 0;
 }
